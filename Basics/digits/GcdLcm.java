5018aafe15afdba93bc16112059e93a10c3eef34
lakshita15 <lakshitamahajan2001@gmail.com>
2022-01-08 18:27:45 +0530

    revision day 2

--- Basics/digits/GcdLcm.java
+++ Basics/digits/GcdLcm.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class GcdLcm {
     public static void main(String[] args) {
      Scanner scn = new Scanner(System.in);
-     int n = scn.nextInt();
-
-    }
+     int divident = scn.nextInt();
+     int divisor =  scn.nextInt();
+     int n1 = divisor;
+     int n2 = divident;
+     while (divisor!=0) {
+         int rem = divident%divisor;
+         divident=divisor;
+         divisor=rem;
+     }
+     System.out.println(divident);
+     //lcm
+     int lcm = (n1 * n2) /  divident;
+     System.out.println(lcm);
+    }    
 }
--- Basics/digits/Inversenumber.java
+++ Basics/digits/Inversenumber.java
@@ -7,5 +7,17 @@ public class Inversenumber {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         
+        int place =1;
+        int inverse =0;
+        while(n!=0){
+            int rem = n%10;
+            int quot = n/10;
+
+            n=quot;
+            inverse= inverse+ place*(int)Math.pow(10, rem-1);
+            place++;
+
+        }
+        System.out.println(inverse);
     }
 }
